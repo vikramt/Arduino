f9d72e2eb98e13b07c6732c50593d4d5cd9d194b
vikramt <viktar@gmail.com>
2014-03-12 15:23:05 -0400

    sender2 done

--- Source/sump/sender2/sender2.ino
+++ Source/sump/sender2/sender2.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <RFM69.h> //get it here: http://github.com/lowpowerlab/rfm69
 #include <SPIFlash.h> //get it here: http://github.com/lowpowerlab/spiflash
+#include <LowPower.h>
 #include <avr/wdt.h> //comes with Arduino
 #include <CFGCMDS.h>
 
@@ -19,6 +20,7 @@ int curRadiotemp, prevRadiotemp=0;
 int curData0, prevData0=0;
 int txErrors=0;
 int txGood=0;
+long curmillis,listenmillis=0;
 
 byte doTransmit=0;
 
@@ -33,7 +35,7 @@ CFGCMDS cfgcmds; //instantiate the class
 #define SERIAL_BAUD 115200
 
 int TRANSMITPERIOD = 900; //transmit a packet to gateway so often (in ms)
-char payload[60] ;
+char payload[60] = {"aBc"};
 char buff[20];
 byte sendSize=0;
 byte recvSENDERID=0;
@@ -84,12 +86,12 @@ void setup() {
 		Serial.print("radiopower: ");
 		Serial.println(cfgcmds.getradiopower(),DEC);
 	}
+   
 	radio.initialize(cfgcmds.getfrequency(),cfgcmds.getnodeID(),cfgcmds.getnetworkID());
 	if ( cfgcmds.getisHW() ) {
 		radio.setHighPower(); //uncomment only for RFM69HW!
 	}	
 	radio.encrypt(cfgcmds.getencryptionKey());
-	
 	radio.setPowerLevel(cfgcmds.getradiopower());
 	
 	
@@ -106,22 +108,27 @@ long lastPeriod = -1;
 
 void loop() {
 	
-	curDistance=getDistance();
+	//curDistance=getDistance();
+   curDistance++;
 	curRadiotemp=radio.readTemperature(cfgcmds.gettempcalibration());
-	doTransmit=0;
-	if ( (cfgcmds.getxmitchange() & bitVoltage ) && (curVoltage - prevVoltage)) { 
-		doTransmit=1; 
+	doTransmit=1;
+   
+   
+   //IF xmit change is 1 then hold back transmit if 0 then transmit on loop
+	if ( (cfgcmds.getxmitchange() & bitVoltage ) && (curVoltage != prevVoltage)) { 
+		doTransmit=0; 
 		prevVoltage=curVoltage;
 	}
-	if ( (cfgcmds.getxmitchange() & bitRadiotemp ) && (curRadiotemp - prevRadiotemp)) { 
-		doTransmit=1; 
+	if ( (cfgcmds.getxmitchange() & bitRadiotemp ) && (curRadiotemp != prevRadiotemp)) { 
+		doTransmit=0; 
 		prevRadiotemp=curRadiotemp;
 	}
-	if ( (cfgcmds.getxmitchange() & bitData0 ) && (curDistance - prevDistance)) { 
-		doTransmit=1; 
-		prevData0=curData0;
+	if ( (cfgcmds.getxmitchange() & bitData0 ) && (curDistance != prevDistance)) { 
+		doTransmit=0; 
+		prevDistance=curDistance;
 	}
-	minxmit--;
+	
+   minxmit--;
 	if ( minxmit <=0 ) { 
 		minxmit = ( cfgcmds.getxmitmin() * 60 )/( cfgcmds.getsleepseconds() ) ;  //reset minxmit countdown to cyles of sleepseconds
 		doTransmit = 1 ;
@@ -145,67 +152,93 @@ void loop() {
     }
 	
   }
-
-  //check for any received packets S:1,126,0 S:2,18,0 
-  if (radio.receiveDone())
-  { 
-	if ( radio.DATALEN > 0 ) { 
-		// if we have data copy it first
-		memcpy(&recvRequest,(void*) &radio.DATA,sizeof(recvRequest)); 
-		recvSENDERID = radio.SENDERID ;
-		recvDATALEN = radio.DATALEN ;
-	}
-	// then send ack if requested to free up sender
-	if (radio.ACK_REQUESTED)
-    {
-      radio.sendACK();
-      Serial.print(" - ACK sent");
-      delay(1);
-    }
-	radio.sleep();// can we sleep after sending ack
-    Serial.print('[');Serial.print(recvSENDERID, DEC);Serial.print("] ");
-	//for (byte i = 0; i < radio.DATALEN; i++)
-	//  Serial.print(radio.DATA[i]);
-	
-    if ( recvDATALEN > 0 ) { //check for datalen cause it maybe just an ACK request etc and radio.data maybe stale
-        memset( &recvRequest,0,sizeof(recvRequest)); // can we zero the request like this
-        if ( recvRequest.nodeID == cfgcmds.getnodeID()  ) {
-            Serial.print("command is : "); Serial.println(recvRequest.command);
-            Serial.print("parameter is :"); Serial.println(recvRequest.parameter);
-        } else {
-			Serial.print("To nodeid: "); Serial.println (recvRequest.nodeID);
-        }
-    }
-    Serial.print(" [RX_RSSI:");Serial.print(recvRSSI);Serial.print("]");
-	
-	
-    Blink(LED,5);
-    Serial.println();
-  }
   
   if ( doTransmit ) {
 	
-	doTransmit = 0;// now we should send data
-	// fill payload here
 	
-	if (radio.sendWithRetry(GATEWAYID, payload, sendSize,1,50)) {
+	// fill payload here and send data
+   sprintf(payload,"SMP V%d, T%d, D%d", curVoltage, curRadiotemp, curDistance);
+	
+	if (radio.sendWithRetry(GATEWAYID, payload, sizeof(payload),1,50)) {
     
        Serial.print(" TX ok: ");
-	   Serial.print(txGood++);
+	   Serial.println(txGood++);
 	}
     else {
        
-	   Serial.print(" TX Errors: "); 
-	   Serial.print(txErrors++);
+	   Serial.print(" TX Error: "); 
+	   Serial.println(txErrors++);
 	}
 	   
-	Serial.println();
-    Blink(LED,3);
+	//Serial.println();
+   // Blink(LED,3);
   }
+
+   // listen for listen100ms durations after the transmit
+   curmillis=millis();
+   listenmillis=100 * cfgcmds.getlisten100ms() ;
+   //listenmillis=5000;
+   
+   while ( millis() < ( curmillis + listenmillis)   ) 
+   {
+      //Serial.println("Now listening");
+      //check for any received packets S:2,126,0 S:2,20,0 
+      if (radio.receiveDone())
+      { 
+         memset( &recvRequest,0,sizeof(recvRequest)); // can we zero the request like this
+         recvSENDERID=0; recvDATALEN=0;
+         
+         if ( radio.DATALEN > 0 ) { 
+            // if we have data copy it first
+            memcpy(&recvRequest,(void*) &radio.DATA,sizeof(recvRequest)); 
+            recvSENDERID = radio.SENDERID ;
+            recvDATALEN = radio.DATALEN ;
+         }
+         // then send ack if requested to free up sender
+         if (radio.ACK_REQUESTED)
+         {
+            radio.sendACK();
+            Serial.print(" - ACK sent");
+            delay(1);
+         }
+         radio.sleep();// can we sleep after sending ack
+         Serial.print('[');Serial.print(recvSENDERID, DEC);Serial.print("] ");
+         //for (byte i = 0; i < radio.DATALEN; i++)
+         //  Serial.print(radio.DATA[i]);
+         
+         if ( recvDATALEN > 0 ) { //check for datalen cause it maybe just an ACK request etc and radio.data maybe stale
+            
+            if ( recvRequest.nodeID == cfgcmds.getnodeID()  ) {
+                  Serial.print("command is : "); Serial.println(recvRequest.command);
+                  Serial.print("parameter is :"); Serial.println(recvRequest.parameter);
+            } else {
+               Serial.print("To nodeid: "); Serial.println (recvRequest.nodeID);
+            }
+         }
+         Serial.print(" [RX_RSSI:");Serial.print(recvRSSI);Serial.print("]");
+         
+         if( recvRequest.command > 0 && recvRequest.nodeID == cfgcmds.getnodeID() ) {
+            Serial.print("Executing cmmand: "); Serial.println(recvRequest.command); 
+            Serial.print("Parameter is: "); Serial.println(recvRequest.parameter);
+            
+            doCommand(recvRequest.command,recvRequest.parameter);
+            recvRequest.command=0;
+         }
+         
+         Blink(LED,5);
+         Serial.println();
+         break; // if tx  received break out of listen mode
+      }
   
-  // listen for listen100ms durations after the transmit
+   }
   
-  // sleep for sleepseconds durations
+
+    radio.sleep();
+    byte s = cfgcmds.getsleepseconds() ; 
+    while ( --s ) {
+      LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+      // sleep for sleepseconds durations
+   }
   
 
 }
@@ -242,4 +275,63 @@ int getDistance() {
   duration = pulseIn(ECHO, HIGH);
   distance = (duration/2) / 29.1;
   return distance;
+}
+
+void doCommand(byte command, byte parameter )
+{
+   long localdata ;
+ 
+ 			if ( command  ) {
+				//command should be greater than zero
+				switch(command)
+				{
+					case READtemp: 	
+                                 Serial.print ("Radio Temp:") ; 
+                                 Serial.println(curRadiotemp);
+                                 payload[0]=0;
+                                 sprintf(payload,"T%d",curRadiotemp );
+                                 radio.sendWithRetry(GATEWAYID, payload, sizeof(payload),1,50) ;                            
+                  break ;
+                  
+					case READvoltage: 	
+                                 Serial.print ("BatVoltage") ; 
+                                 Serial.println(curVoltage);
+                                 payload[0]=0;
+                                 sprintf(payload,"V%d",curVoltage );
+                                 radio.sendWithRetry(GATEWAYID, payload, sizeof(payload),1,50);                                              
+                  break ;
+
+					case READdata0: 	
+                                 Serial.print ("Data0: ") ; 
+                                 Serial.println(curDistance);
+                                 payload[0]=0;
+                                 sprintf(payload,"D%d",curDistance );
+                                 radio.sendWithRetry(GATEWAYID, payload, sizeof(payload),1,50)  ;                                            
+                  break ;
+					
+					case WRITEtempcalibrationreq: 	    
+                                 Serial.println("Set tempcalib"); 
+                                 cfgcmds.settempcalibration(parameter); 
+                  break ;
+					
+					case WRITEradiopowerreq: 			
+                                 Serial.println("Set Radiopower"); 
+                                 cfgcmds.setradiopower(parameter); 
+                  break ;
+					
+					case WDTREBOOT :					
+                                 Serial.println("Rebooting node"); 
+                                 WDTReboot(); 
+                  break ;
+				
+				}
+					
+			
+			}
+		   else {
+
+			Serial.print("Not Executing ..  Command: ");
+		  }  
+        
+   
 }
